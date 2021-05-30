0f7f8271f20772c4f49b0e7193f08a41a612ec10
sivanath peddakotla <speddak1@my.westga.edu>
2021-05-30 14:48:44 -0500

    Update testBrickConstructor.js

--- test/testBrickConstructor.js
+++ test/testBrickConstructor.js
@@ -4,9 +4,16 @@ const Brick = lego.Brick;
 describe('Brick', function() {
     describe('#instanceof()', function() {
       it('should return 1 when the brick object exists', function() {
-          brick = new Brick(1, "Red");
+          var brick = new Brick(1, "Red");
           assert.equal(brick.size , 1 , " No Brick object");
         
       });
     });
+    describe('#Notinstanceof()', function() {
+        it('should not create brick object ', function() {
+            var brick = new Brick("Red", 1);
+            assert.equal(brick.size , 1 , " No Brick object");
+          
+        });
+      });
   });
