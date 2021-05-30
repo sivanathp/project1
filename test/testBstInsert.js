9a98dbb1bb3a960cf8480293489aeaaa053311b0
sivanath peddakotla <speddak1@my.westga.edu>
2021-05-30 15:23:15 -0500

    added one more test

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -15,5 +15,18 @@ describe('BST', function() {
         
       });
     });
+
+    describe('root plus a left child', function() {
+        it('a single Node and left child at the root of the BST', function() {
+            var brick0 = new Brick(5, "Red");
+            var tree = new BST();
+            tree.insert(brick0);
+            var brick1 = new Brick(3, "Red");
+            tree.insert(brick1);
+            assert.equal(tree.root.data, brick0);
+            assert.equal(tree.root.left.data, brick1);
+          
+        });
+      });
    
   });
