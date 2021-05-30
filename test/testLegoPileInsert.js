b80df7785c8ee654d56bb472660b2899870b773b
sivanath peddakotla <speddak1@my.westga.edu>
2021-05-30 16:38:32 -0500

    Added another test

--- test/testLegoPileInsert.js
+++ test/testLegoPileInsert.js
@@ -14,10 +14,34 @@ describe('LegoPile', function() {
           pile.insert(brick0);
           var bst = pile.datastore["red"]
           assert.equal(bst.root.data, 1);
-
         
       });
     });
  
-   
+    describe('insert one brick of each color', function() {
+      it('A single brick of each color in lego pile', function() {
+          var brick0 = new Brick(1, "red");
+          var brickgreen = new Brick(2, "green");
+          var brickblue = new Brick(3, "blue");
+          var brickyellow = new Brick(4, "yellow");
+          var brickblack = new Brick(5, "black");
+          var brickwhite = new Brick(6, "white");
+
+          var pile = new LegoPile();
+          pile.insert(brick0);
+          pile.insert(brickgreen);
+          pile.insert(brickblue);
+          pile.insert(brickyellow);
+          pile.insert(brickblack);
+          pile.insert(brickwhite);
+          var bst = pile.datastore["red"]
+          assert.equal(bst.root.data, 1);
+          assert.equal(pile.datastore["green"].root.data, 2);
+          assert.equal(pile.datastore["blue"].root.data, 3);
+          assert.equal(pile.datastore["yellow"].root.data, 4);
+          assert.equal(pile.datastore["black"].root.data, 5);
+          assert.equal(pile.datastore["white"].root.data, 6);
+        
+      });
+    });
   });
